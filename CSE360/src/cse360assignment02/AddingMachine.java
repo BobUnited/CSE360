2a3eb82facc782bb00e265008a44f94c79ae9ca1
BobUnited <sjslayto@asu.edu>
2020-10-04 20:55:29 -0700

    Initial commit.

---
+++ CSE360/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,50 @@
+package cse360assignment02;
+
+/**
+ * A class that can add and subtract.
+ */
+public class AddingMachine {
+	/**
+	 * The current total of the adding machine.
+	 */
+	  private int total;
+	  
+	  /**
+	   * Constructor of AddingMachine, sets total to 0.
+	   */
+	  public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  /**
+	   * Returns the total of the adding machine.
+	   * @return This AddingMachine's total.
+	   */
+	  public int getTotal () {
+	    return 0;
+	  }
+	  /**
+	   * Adds value to this machine's total
+	   * @param value The value to add.
+	   */
+	  public void add (int value) {
+	  }
+	  /**
+	   * Subtracts value from this machine's total
+	   * @param value The value to subtract.
+	   */
+	  public void subtract (int value) {
+	  }
+	  /**
+	   * Returns a string representation of the adding machine.
+	   * @return The string representation of the adding machine.
+	   */
+	  public String toString () {
+	    return "";
+	  }
+
+	  /**
+	   * Sets the value of the machine to 0.
+	   */
+	  public void clear() {
+	  }
+	}
